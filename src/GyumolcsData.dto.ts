80037fcd3df2bc50bbd588790d0204e8e22daa0d
zmarc3ll <zubermarcellpetrik@gmail.com>
2023-02-14 08:50:58 +0100

    addign class validations

--- src/GyumolcsData.dto.ts
+++ src/GyumolcsData.dto.ts
@@ -1,3 +1,4 @@
+import { IsInt, IsString, Length, Min } from "class-validator";
 import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity()
@@ -7,8 +8,12 @@ export default class GyumolcsData {
     id: number;
 
     @Column()
+    @Length(1,15)
+    @IsString()
     nev: string;
 
     @Column()
+    @Min(0)
+    @IsInt()
     darab:  number;
 }
